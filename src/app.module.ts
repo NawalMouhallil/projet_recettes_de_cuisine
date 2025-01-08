7667feb59378b1f9482a9a7b19906c96375843b1
Constantin Maes <constantinmaes@gmail.com>
2025-01-08 19:52:11 +0100

    feat(database): integrate Mongoose for MongoDB connection and update dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,30 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { Config } from './../node_modules/cosmiconfig/dist/types.d';
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UsersModule],
+  imports: [
+    ConfigModule.forRoot(),
+    UsersModule,
+    /*MongooseModule.forRoot('mongodb://root:root@localhost:27017/sgbd', {
+      authSource: 'admin',
+    }),*/
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        return {
+          uri: `mongodb://${configService.get('MONGO_DB_USER')}:${configService.get('MONGO_DB_PASSWORD')}@${configService.get('MONGO_DB_HOST')}:${configService.get('MONGO_DB_PORT')}/${configService.get('MONGO_DB_NAME')}`,
+          authSource: 'admin',
+        };
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
