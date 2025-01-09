d5c1238112c43ec1ab7c0870fe49b53f6e9aef73
Constantin Maes <constantinmaes@gmail.com>
2025-01-09 21:04:08 +0100

    feat(user): implement user creation and management with Mongoose integration

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,15 +1,41 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { CreateUserDto } from './dto/createUser.dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(private usersService: UsersService) {}
+
   @Get()
   getAll() {
-    return [];
+    return this.usersService.find();
+  }
+
+  @Get(':id') // http://localhost:3000/users/abcd
+  getById(@Param('id') id: string) {
+    return this.usersService.findById(id);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.usersService.delete(id);
   }
 
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
-    return createUserDto;
+    return this.usersService.create(createUserDto);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() createUserDto: CreateUserDto) {
+    return this.usersService.update(id, createUserDto);
   }
 }
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserSchema } from './user.schema';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
 
 @Module({
-  controllers: [UsersController]
+  imports: [MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])],
+  controllers: [UsersController],
+  providers: [UsersService],
 })
 export class UsersModule {}
