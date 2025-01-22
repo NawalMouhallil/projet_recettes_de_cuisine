158a2063ccf6e82080860e279a9d8262d7befaec
Constantin Maes <constantinmaes@gmail.com>
2025-01-22 20:19:52 +0100

    feat(order): add order schema and integrate with user service for aggregation

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { OrderSchema } from './order.schema';
 import { UserSchema } from './user.schema';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])],
+  imports: [
+    MongooseModule.forFeature([
+      { name: 'User', schema: UserSchema },
+      { name: 'Order', schema: OrderSchema },
+    ]),
+  ],
   controllers: [UsersController],
   providers: [UsersService],
 })
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,18 +1,38 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from './user.schema';
-import { Model } from 'mongoose';
+import { Model, Types } from 'mongoose';
 
 @Injectable()
 export class UsersService {
+  private ordersPipelineStage = {
+    $lookup: {
+      from: 'orders',
+      localField: '_id',
+      foreignField: 'userId',
+      as: 'orders',
+    },
+  };
   constructor(@InjectModel('User') private userModel: Model<User>) {}
 
   async find() {
-    return await this.userModel.find();
+    return await this.userModel.aggregate([this.ordersPipelineStage]);
   }
 
   async findById(id: string) {
-    return await this.userModel.findById(id);
+    // return await this.userModel.findById(id);
+    const users = await this.userModel.aggregate([
+      { $match: { _id: new Types.ObjectId(id) } },
+      {
+        $lookup: {
+          from: 'orders',
+          localField: '_id',
+          foreignField: 'userId',
+          as: 'orders',
+        },
+      },
+    ]);
+    return users[0];
     // return await this.userModel.findOne({ _id: id });
   }
 
