abf45558ca2022632adef81d561a86e5bf9c0892
Nawal <omowalaa@gmail.com>
2025-02-07 18:18:30 +0100

    commit...

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: 'tsconfig.json',
+    project: ['tsconfig.json', 'tsconfig.dist.json'],
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
